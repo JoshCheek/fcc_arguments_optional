8932738ef6f8247c8f41814f8af999837f9227b8
Josh Cheek <josh.cheek@gmail.com>
2016-05-16 16:50:17 -0500

    Some examples of chai assertions and arrays (use deepEqual)

--- test/test.js
+++ test/test.js
@@ -3,8 +3,19 @@ var assert = require('chai').assert;
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+      assert.equal(-1, ['a', 'b', 'c'].indexOf('e'));
+      assert.equal(-1, ['a', 'b', 'c'].indexOf('x'));
+    });
+  });
+
+  describe('equality', function() {
+    it('is not equal to an array with the same values', function() {
+      assert.notEqual(['a','b','c'], ['a','b','c']);
+    });
+
+    it('can be compared in chai with deepEqual', function() {
+      assert.deepEqual(   ['a','b','c'], ['a','b','c']);
+      assert.notDeepEqual(['a','b','x'], ['a','b','c']);
     });
   });
 });
